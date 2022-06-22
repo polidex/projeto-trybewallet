bd8cedc5e3a70a65451496517e04f3bf53ab3680
polidex <lp.igor@gmail.com>
2022-06-22 15:10:27 -0300

    [Igor Polido] iniciando o projeto TrybeWallet #VQV!

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Login</h1>
+        <input
+          type="email"
+          placeholder="E-mail"
+          data-testid="email-input"
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          data-testid="password-input"
+        />
+        <br />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
