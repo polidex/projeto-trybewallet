326be13a7ff9fd8ee76293a3decdab03688c8121
polidex <lp.igor@gmail.com>
2022-07-02 18:22:38 -0300

    7 done

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Form from './Form';
 import Header from './Header';
+import Table from './Table';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Form />
+        <Table />
         TrybeWallet
-      </div>);
+      </div>
+    );
   }
 }
 
